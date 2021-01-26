7eb339b37a881d7215a01a5e287f667784f8a46a
Abeco Jean Claude <73258089+abecojc@users.noreply.github.com>
2021-01-26 13:45:09 -0500

    readme edit

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# codeofhonors-jobs-tracker-v1v2
-Repo for Jobs Tracker V1V2 Sprint 5 project
+---
+marp: true
+---
+
+# Great Horizons
+## A job search with functionalities:
+- keyword search
+- filters
+- create an account
+- sign in
+- explore resources
+- view job listings
+- interact with specific job
+- save favorite jobs
+- share jobs
+
+ 
+ -----
+ ## Tools:
+ - github (screenshot)
+ - vercel (screenshot)
+ - firebase (screenshot)
+ - adzuna API (screenshot)
+ - webstorm (screenshot)
+ - zenhub (screenshot)
+
+
+
+
+ -------
+ ## Balsamic : wireframe
+ (screenshot)
+
+
+
+
+ ------
+ ## Whimsical : user journey
+ (screenshot)
+
+
+
+ -------
+ ## Firebase : authentication / storage
+ (screenshot)
+
+
+
+ --------
+ ## Adzuna : jobs API
+ (screenshot)
+
+
+
+
+ ---------
+ ## Demo: webstorm
+
+
+
+
+ --------
+ ## Demo: github
+
+
+
+
+ ---------
+ ## Demo: vercel 
+
+
+
+
+ -----
+
+ 
--- search-own.js
+++ search-own.js
@@ -1,22 +1,30 @@
+// APP_ID = d9bee79e
+//APP_KEY = a8eea83bb5dbb169da71decb4c8cac85
 const searchForm = document.querySelector('#search-form')
 const jobType = document.querySelector('#job-type')
 const resultSection = document.querySelector('#result-section')
 const searchHeader = document.querySelector('#app')
+
 const SEARCH_COUNTRY = 'us'
 //adds
 // const searchCity = document.querySelector('#location1')
 const searchFull = document.querySelector('#form')
 const keyword = document.querySelector('#keyword')
 const searchFormFull = document.querySelector('#search-page')
-const like = document.querySelector('#favorite')
+
+let likes = 0
 const allLikes = []
 
-function likes(){
-  allLikes.push(like)
-  console.log(allLikes)
+function onClick(){
+  likes += 1;
+  document.querySelector('#favorite')
+  // allLikes.push(likeCount)
+  // console.log(allLikes)
 }
+console.log(likes)
+
 searchFull.addEventListener('submit', async function (e) {
-  e.preventDefault()
+   e.preventDefault()
 
   const keyName = keyword.value
 
@@ -31,7 +39,7 @@ searchFull.addEventListener('submit', async function (e) {
   resultSection.innerHTML = `
 
   <div id="result-section" class="p-10">
-  
+  <h2 class=" text-xl text-white shadow-lg text-center rounded p-2">
       <h1>${keywordsFound} jobs found for <strong>${keyName}</strong> in ${SEARCH_COUNTRY.toUpperCase()}</h1>
   
   </div>`
@@ -40,13 +48,17 @@ searchFull.addEventListener('submit', async function (e) {
     const div = document.createElement('div')
     div.innerHTML = `
        <div class="rounded cursor-pointer border-2 border-blue-900 shadow-lg my-2 hover:bg-blue-400">
-          <h4 class="p-2 border-2 border-blue-400 bg-blue-500 text-white whitespace-nowrap shadow-2xl">
+          <h4 class="p-2 border-2 border-yellow-200 bg-blue-500 text-white whitespace-nowrap shadow-2xl">
                 <a href="${key.redirect_url}">
                        ${key.title}
                 </a> | ${key.location.display_name} 
+                <p>${key.location.area[0]}</p>
+                <p>${key.company.display_name}</p>
+                <p>${key.category.label}</p>
           </h4>
-           <p  class="p-2">
+           <p  class="p-2 text-white ">
                ${key.description}
+           
            </p>
        </div>`
     resultSection.appendChild(div)
@@ -59,7 +71,7 @@ searchFull.addEventListener('submit', async function (e) {
 //end adds
 
 searchForm.addEventListener('submit', async function (e) {
-  e.preventDefault()
+   e.preventDefault()
 
   const jobName = jobType.value
 
@@ -74,7 +86,7 @@ searchForm.addEventListener('submit', async function (e) {
   resultSection.innerHTML = `
 
   <div id="result-section mt-56" class="p-10">
-      <h2 class=" text-xl bg-blue-400 shadow-lg text-center rounded p-2">Search results</h2>
+      <h2 class=" text-xl bg-black shadow-lg text-center rounded p-2">Search results</h2>
       <h1 class="text-center rounded p-2 text-xl bg-blue-400 text-white">${jobsFound} jobs found for <strong>${jobName}</strong> in ${SEARCH_COUNTRY.toUpperCase()}</h1> 
   </div>`
 
@@ -87,15 +99,16 @@ searchForm.addEventListener('submit', async function (e) {
                 <a href="${job.redirect_url}">
                        ${job.title}
                  ---------- ${job.location.display_name} </a> 
-                 
+                 <p>${job.salary_max}</p>
                   <div>
-                   <a href=""><i class="far fa-heart bg-white text-red-400" id="favorite">Like</i></a>                     
+                  <button type="button" id="favorite" onClick="onClick()"><i class="fas fa-thumbs-up"></i>Favorite</button>
+<!--                    <p>Likes: <a >0</a></p>-->
+                                 
                    <a class="twitter-share-button bg-white text-blue-800" href="https://twitter.com/intent/tweet">Tweet</a>
-                   <link rel="canonical" href="/web/tweet-button">
+<!--                   <link rel="canonical" href="/web/tweet-button">-->
                  </div>
                 
-                
-                
+                       
           </h4>
            <p  class="p-2">
                ${job.description}
@@ -109,7 +122,20 @@ searchForm.addEventListener('submit', async function (e) {
   })
 })
 // 92b62ee9bfd90c11c097004b51438beb
-async function searchJobs (jobString, jobsCount = 8, country = SEARCH_COUNTRY) {
+
+let allJobs = searchJobs
+let JustOver40k = []
+allJobs.filter = function JustOver40k(job){
+  (job.salary_min >= 40000)
+}
+
+JustOver40k.forEach((job) =>{
+  const div = document.createElement('div')
+  div.innerHTML = `Hello`
+
+})
+
+async function searchJobs (jobString, jobsCount = 2, country = SEARCH_COUNTRY) {
 
   const url = `https://api.adzuna.com/v1/api/jobs/${country}/search/1?app_id=d9bee79e&app_key=a8eea83bb5dbb169da71decb4c8cac85&results_per_page=${jobsCount}&what=${jobString}&content-type=application/json`
   console.log(url)
